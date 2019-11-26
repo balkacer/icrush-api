4bab4ff4b22bd4ae7c4fb9219762710b98c3e713
Enmah4k <enmah4k@gmail.com>
2019-11-26 04:53:55 -0400

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCrxMuDrX7kISiuL4vlmxUBgD4ct6u9CFQ",
+    authDomain: "dat-icrush.firebaseapp.com",
+    databaseURL: "https://dat-icrush.firebaseio.com",
+    projectId: "dat-icrush",
+    storageBucket: "dat-icrush.appspot.com",
+    messagingSenderId: "748541358648",
+    appId: "1:748541358648:web:77c139e4a10ef1e126accd",
+    measurementId: "G-H4T0S5C2F6"
+};
